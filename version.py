603d5ea45736f6b653ced9d0abe12c3b823ac876
Jikhan Jung <honestjung@gmail.com>
2025-09-08 12:15:49 +0900

    chore: bump version to 0.2.1

--- version.py
+++ version.py
@@ -2,5 +2,5 @@
 CTHarvester Version Information
 Single Source of Truth for version management
 """
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __version_info__ = tuple(map(int, __version__.split('.')))
